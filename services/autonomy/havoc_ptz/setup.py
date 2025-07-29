9576d7f4f4142b6ae4fe20d9504c371595629033
Kevin Becker <kevin.becker@havocai.com>
2025-07-29 13:56:50 -0400

    Compiling thigns

--- services/autonomy/havoc_ptz/setup.py
+++ services/autonomy/havoc_ptz/setup.py
@@ -1,23 +1,24 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'havoc_ptz'
 
 setup(
     name=package_name,
     version='0.1.0',
-    # packages=[package_name],
+    packages=find_packages(exclude=['test']),
     package_dir={'.': 'src'},
-    # data_files=[
-    #     ('share/' + package_name, ['package.xml']),
-    #     ('share/ament_index/resource_index/packages', [package_name]),
-    #     ('share/' + package_name + '/launch', ['/launch/ptz_launch.py']),
-    # ],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools', 'onvif-zeep', 'lxml', 'pygame', 'numpy', 'zeep'],
     zip_safe=True,
     maintainer='Your Name',
     maintainer_email='you@example.com',
     description='PTZ control node with TF and prediction',
     license='MIT',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'ptz_node = havoc_ptz.ptz_node:main',
